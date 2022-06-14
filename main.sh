ffae9ed757f43155bfc3e126d2b61a5075e2f681
Nitin Sureka <surekanitin@gmail.com>
2022-06-15 00:18:14 +0530

    New changes in features

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# yum install python3
-# yum install pip3
-# pip3 install requirements.txt
+yum install python3
+yum install pip3
+pip3 install requirements.txt
 python /home/nitin/Desktop/PROJECT/Telegram-Bot/Bots/bot.py &
 python /home/nitin/Desktop/PROJECT/Telegram-Bot/Bots/covid_bot.py &
 python /home/nitin/Desktop/PROJECT/Telegram-Bot/Bots/stock_bot.py &
